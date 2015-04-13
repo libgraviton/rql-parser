0e2f950d77163c7eff031ed9041d530ca8a5e352
evtushenko <alex.the.mrix@gmail.com>
2015-04-13 23:53:54 +0600

    Add syntax error exception.

---
+++ src/Exception.php
@@ -0,0 +1,8 @@
+<?php
+namespace Mrix\Rql\Parser;
+
+/**
+ */
+interface Exception
+{
+}
---
+++ src/Exception/SyntaxErrorException.php
@@ -0,0 +1,10 @@
+<?php
+namespace Mrix\Rql\Parser\Exception;
+
+use Mrix\Rql\Parser\Exception;
+
+/**
+ */
+class SyntaxErrorException extends \LogicException implements Exception
+{
+}
