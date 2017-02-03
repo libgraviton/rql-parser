6770eecef6c51bb8806be5a345ed72e163c08b29
Derrek Bertrand <derrekbertrand@gmail.com>
2017-02-03 12:48:52 -0500

    Fixed some CS issues

--- src/SubLexer/StringSubLexer.php
+++ src/SubLexer/StringSubLexer.php
@@ -12,19 +12,21 @@ class StringSubLexer implements SubLexerInterface
     public function getTokenAt($code, $cursor)
     {
         //if it doesn't match a string, return null
-        if (!preg_match('/([a-z0-9_.]|\%[0-9a-f]{2})+/Ai', $code, $matches, null, $cursor))
+        if (!preg_match('/([a-z0-9_.]|\%[0-9a-f]{2})+/Ai', $code, $matches, null, $cursor)) {
             return null;
+        }
 
         //if it did match a string, ensure that the same string would not match a number
         $num_token = (new NumberSubLexer())->getTokenAt($code, $cursor);
+
         //if we could just as well find a number here
-        if($num_token instanceof Token)
-        {
+        if ($num_token instanceof Token) {
             $str_len = ($cursor + strlen($matches[0])) - $cursor;
             $num_len = $num_token->getEnd() - $num_token->getStart();
             // finding '2017' in '2017-01-01' is not good enough
-            if($num_len >= $str_len)
+            if ($num_len >= $str_len) {
                 return $num_token;
+            }
         }
 
         return new Token(
