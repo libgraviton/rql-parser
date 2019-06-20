953a47e9f1f27b3ebf083610d62ed94a713ee387
Dario Nuevo <wee@xbe.ch>
2019-06-20 11:42:07 +0200

    toRql() is now abstract

--- src/AbstractNode.php
+++ src/AbstractNode.php
@@ -7,4 +7,9 @@ abstract class AbstractNode
      * @return string
      */
     abstract public function getNodeName();
+
+    /**
+     * @return string
+     */
+    abstract public function toRql();
 }
