c2df75ebf29699f211a4a85c8c5d46ea58b5d0a3
Dario Nuevo <wee@xbe.ch>
2023-03-13 13:35:34 +0100

    new base class

---
+++ src/NodeParser/PrefixedListNodeParser.php
@@ -0,0 +1,64 @@
+<?php
+namespace Graviton\RqlParser\NodeParser;
+
+use Graviton\RqlParser\Token;
+use Graviton\RqlParser\TokenStream;
+use Graviton\RqlParser\NodeParserInterface;
+use Graviton\RqlParser\Node\SortNode;
+use Graviton\RqlParser\SubParserInterface;
+
+abstract class PrefixedListNodeParser implements NodeParserInterface
+{
+
+    protected SubParserInterface $fieldNameParser;
+
+    /**
+     * @param SubParserInterface $fieldNameParser
+     */
+    public function __construct(SubParserInterface $fieldNameParser)
+    {
+        $this->fieldNameParser = $fieldNameParser;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function parse(TokenStream $tokenStream)
+    {
+        $fields = [];
+
+        $tokenStream->expect(Token::T_OPERATOR, $this->getNodeName());
+        $tokenStream->expect(Token::T_OPEN_PARENTHESIS);
+
+        do {
+            $prefix = $tokenStream->expect($this->getAllowedPrefixes());
+            $value = $this->fieldNameParser->parse($tokenStream);
+
+            $fields = $this->addField($fields, $prefix, $value);
+
+            if (!$tokenStream->nextIf(Token::T_COMMA)) {
+                break;
+            }
+        } while (true);
+
+        $tokenStream->expect(Token::T_CLOSE_PARENTHESIS);
+
+        return new SortNode($fields);
+    }
+
+    public function getAllowedPrefixes() : array {
+        return [Token::T_PLUS, Token::T_MINUS];
+    }
+
+    abstract function getNodeName() : string;
+
+    abstract function addField(array $fields, Token $prefix, string $value) : array;
+
+    /**
+     * @inheritdoc
+     */
+    public function supports(TokenStream $tokenStream)
+    {
+        return $tokenStream->test(Token::T_OPERATOR, $this->getNodeName());
+    }
+}
